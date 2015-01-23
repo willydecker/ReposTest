eb86c12ce7a87adbcd562a19647c5e85c159e388
willydecker <wdw1062@saintjoe.edu>
2015-01-23 09:47:03 -0500

    THis is the running code of Resursion in Ch3#

---
+++ RT.js
@@ -0,0 +1,17 @@
+function isEven(x) {
+		if (x == 0) {
+			return true;
+		}	
+		else if (x == 1) {
+		       	return false;
+		}
+		else if (x < 0) {
+			return isEven(-x);
+		}
+		else
+			return isEven(x-2);
+}
+
+console.log(isEven(4));
+console.log(isEven(5));
+console.log(isEven(-2));
